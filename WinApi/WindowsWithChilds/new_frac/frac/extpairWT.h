8281c90d0d1241e5f47c40089e2e5ce052c981b7
DenisMath <morozov.den@samsung.com>
2012-12-05 14:50:38 +0200

    stable version and fix bag with trackers

---
+++ WinApi/WindowsWithChilds/new_frac/frac/extpairWT.h
@@ -0,0 +1,98 @@
+#ifndef EXTPAIRWT
+#define EXTPAIRWT
+#include<list>
+#include<algorithm>
+#include<math.h>
+#include "extpair.h"
+
+//using namespace std;
+
+inline
+	float maxN(float x, float y)
+{ 
+	return ((x>y) ? x : y) ;
+}
+
+class ExtpairWT{
+public:
+	ExtpairWT( )
+	{ }
+
+	ExtpairWT( Extpair e, float t):extpair(e),tension(t)
+	{ 
+	}
+
+	ExtpairWT( float xCoord, float yCoord, float t):extpair(xCoord,yCoord),tension(t)
+	{ 
+	}
+
+
+
+	Extpair extpair;
+	float tension;
+
+	ExtpairWT &operator=(const ExtpairWT &m){
+		extpair =m.extpair;
+		tension = m.tension;
+		tension = m.tension;
+		return *this;
+	}
+
+	ExtpairWT  operator-(){
+		ExtpairWT tmp( -extpair, -tension);
+		return tmp;
+	}
+	 //Extpair new1 = -a1.extpair;
+	friend bool operator==(const ExtpairWT &a1, const ExtpairWT &a2); 
+	friend bool operator<(const ExtpairWT &a1, const ExtpairWT &a2); 
+};
+
+inline
+bool operator==(const ExtpairWT &a1, const ExtpairWT &a2)
+{
+	return (a1.extpair == a2.extpair )&&(a1.tension == a2.tension);
+}
+
+inline
+bool operator<(const ExtpairWT &a1, const ExtpairWT &a2)
+{
+	return ( a1.extpair < a2.extpair ) || ((a1.extpair == a2.extpair)&&(a1.tension < a2.tension));
+}
+
+inline
+ExtpairWT floorExtWT(const ExtpairWT &input)
+{
+	ExtpairWT output( floor(input.extpair.xKoord),floor(input.extpair.yKoord), input.tension );
+		return output;
+}
+
+inline
+bool equalExtpair(const ExtpairWT &a, const ExtpairWT &b)
+{
+	return ( a.extpair == b.extpair );
+}
+
+inline
+
+bool moreExtWT(const ExtpairWT &a, const ExtpairWT &b)
+{
+	return b<a;
+}
+inline
+ void floorListExtWTWeak(std::list<ExtpairWT> &input)
+{
+	for_each(input.begin(),input.end(),floorExtWT);
+	input.sort();
+	input.unique();
+}
+
+inline
+ void floorListExtWTStrong(std::list<ExtpairWT> &input)
+{
+	for_each(input.begin(),input.end(),floorExtWT);
+	//input.sort();
+	input.sort(moreExtWT);
+	input.unique(equalExtpair);
+}
+
+#endif
---
+++ WinApi/WindowsWithChilds/new_frac/frac/resource2.h
@@ -0,0 +1,59 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by main.rc
+//
+#define IDD_MAINDLG                     101
+#define IDD_CHILDDLG                    102
+#define IDD_MAINDLG1                    103
+#define IDC_BUTTON                      1001
+#define IDC_ADD                         1001
+#define IDC_NUMBER                      1002
+#define IDC_EDIT1                       1002
+#define IDC_POINTSNUMBER                1002
+#define IDC_X                           1002
+#define IDC_A22                         1003
+#define IDC_ITERATIONNUMBER             1003
+#define IDC_A21                         1004
+#define IDC_A12                         1006
+#define IDC_A11                         1007
+#define IDC_TENSION                     1008
+#define IDC_Y                           1009
+#define IDC_RANDOM                      1010
+#define IDC_CALIBRATE                   1011
+#define IDC_COMPLETE                    1012
+#define IDC_BUILDING                    1013
+#define IDC_PRINTEPS                    1016
+#define IDC_BUTTON1                     1017
+#define IDC_CHECKEPS                    1018
+#define IDC_TENSIONCOEFF                1019
+#define IDC_COLOR2                      1019
+#define IDC_COLOR3                      1020
+#define IDC_COLOR4                      1021
+#define IDC_COLOR1                      1022
+#define IDC_COLOR5                      1023
+#define IDC_COLOR6                      1024
+#define IDC_COLOR7                      1025
+#define IDC_COLOR8                      1026
+#define IDC_SHIFTCOEFF                  1030
+#define IDC_COLORINTENSE                1031
+#define IDC_GAMMA                       1032
+#define IDC_GAMMACOEF                   1033
+#define IDC_MATRIXTYPE                  1034
+#define IDC_MATRIXTYPE1                 1034
+#define IDC_MATRIXTYPE2                 1035
+#define IDC_MATRIXTYPE3                 1036
+#define IDC_MATRIXTYPE4                 1037
+#define IDC_MATRIXTYPE5                 1038
+#define IDC_MATRIXTYPE6                 1039
+#define IDC_MATRIXTYPE7                 1040
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1035
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
